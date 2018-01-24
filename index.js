fe00aae67a67ecc1cf9e6fc9cbc67e150c307bfc
Elena Reutova <Elena.Reutova@artezio.com>
2018-01-24 19:58:21 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GeneralEvaluateComponent from './App';
+
+ReactDOM.render(<GeneralEvaluateComponent />, document.getElementById('root'));
+
