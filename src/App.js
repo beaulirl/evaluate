db9b2e61ef774b5cdddd228ac9457991debd9edc
Elena <e.reutova94@gmail.com>
2018-02-12 00:45:46 +0300

    Change structure, add more components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import EvaluateComponent from './components/Evaluate.jsx';
 export default class GeneralEvaluateComponent extends Component {
   render() {
     const evaluateData = this.props.evaluateData;
-    const evaluateList = evaluateData.map((ev) => <EvaluateComponent evaluateData={ev}/>);
+    const evaluateList = evaluateData.map((ev) => <EvaluateComponent key={ev.title} evaluateData={ev}/>);
     return (
       <div className="general-eval">
         {evaluateList}
--- src/components/Mark.jsx
+++ src/components/Mark.jsx
@@ -3,11 +3,26 @@ import React, { Component } from 'react';
 import './Mark.css';
 
 export default class Mark extends Component {
+  constructor(props) {
+  	super(props);
+  	this.handleChange = this.handleChange.bind(this);
+  	this.state = {
+  		value: '',
+  	};
+  }
+
+  handleChange(event) {
+  	const values = ['A', 'B', 'C', 'D', 'E', 'F', '']
+  	    if (values.includes(event.target.value)) {
+  	    	this.setState({value: event.target.value});
+  	    }
+  	}
+  
   render() {
     return (
       <div className="mark">
-        <input type="text" className="mark-value" />
-        <div className="mark-description" contenteditable="true"></div>
+        <input type="text" className="mark-value" onChange={this.handleChange} value={this.state.value}/>
+        <div className="mark-description" contentEditable="true"></div>
         <div className="mark-author">/ {this.props.employee}</div>
       </div>
       );
